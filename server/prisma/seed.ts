24a13f418144604a0473b4fc7e60a6577e5062e9
Mark1q <mark.eduard7@gmail.com>
2025-12-20 21:28:56 +0200

    feat: add seeding to prisma db

---
+++ server/prisma/seed.ts
@@ -0,0 +1,24 @@
+import { prisma } from "../lib/prisma";
+
+async function main() {
+  console.log('🌱 Seeding database...')
+
+  const testUser = await prisma.user.upsert({
+    where: { email: 'dev@example.com' }, 
+    update: {}, 
+    create: {
+      email: 'dev@example.com'
+    },
+  })
+
+  console.log(`✅ Created/Verified user: ${testUser.email}`)
+}
+
+main()
+  .catch((e) => {
+    console.error(e)
+    process.exit(1)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })

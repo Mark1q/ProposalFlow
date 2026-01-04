195ca9846c22783b6fb84352d807827a6bd48ca3
Mark1q <mark.eduard7@gmail.com>
2026-01-04 15:57:53 +0200

    chore(auth): add dummy hash for login security

--- server/constants.ts
+++ server/constants.ts
@@ -1,3 +1,9 @@
-const DEMO_ID = "b2294fcb-5c33-4d58-b0e4-bef174f9b32a";
+const DEMO_ID = "73a0a87f-7f82-4ede-8c42-5868f840f397";
 
-export { DEMO_ID }
+/**
+ * Used during login to prevent timing attacks. 
+ * Provides a hash for bcrypt.compare to work against even if the user doesn't exist.
+ */
+const DUMMY_HASH = "$2b$10$C69YVnRAQ9.D.S.N1010101010101010101010101010101010";
+
+export { DEMO_ID, DUMMY_HASH }
